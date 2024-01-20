5b5296e9ad5e155194361a39af5672cf89e026bf
Jomalang <rhkddlr98@naver.com>
2024-01-20 22:57:52 +0900

    240120

---
+++ CH14_Graph/ALGraph.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "ALGraph.h"
+
+int WhoIsPrecede(int data1, int data2); //비교기준
+
+//그래프 초기화 함수
+void GraphInit(ALGraph* pg, int nv)
+{
+	pg->numV = nv;
+	pg->numE = 0;
+	pg->adjList = (List*)malloc(sizeof(List) * nv); // nv * list만큼 공간 할당
+	
+	for (int i = 0; i < nv; i++)
+	{
+		ListInit(&(pg->adjList[i]));
+		SetSortRule(&(pg->adjList[i]), WhoIsPrecede);
+	}
+}
+
+//그래프 리소스 해제 함수
+void GraphDestory(ALGraph* pg)
+{
+	if (pg->adjList != NULL)
+		free(pg->adjList);
+}
+
+//간선 추가 함수
+void AddEdge(ALGraph* pg, int fromV, int toV)
+{
+	LInsert(&(pg->adjList[fromV]), toV);
+	LInsert(&(pg->adjList[toV]), fromV);
+
+	pg->numE++;
+}
+
+//간선 정보 출력 함수
+void ShowGraphEdgeInfo(ALGraph* pg)
+{
+	int vx;
+
+	for (int i = 0; i < pg->numV; i++)
+	{
+		printf("%c와 연결된 정점: ", i + 65);
+
+		if (LFirst(&(pg->adjList[i]), &vx))
+		{
+			printf("%c ", vx + 65);
+			while(LNext(&(pg->adjList[i]), &vx))
+				printf("%c ", vx + 65);
+		}
+		printf("\n");
+	}
+}
+
+
+int WhoIsPrecede(int data1, int data2)
+{
+	if (data1 < data2)
+		return 0;
+	else
+		return 1;
+}
